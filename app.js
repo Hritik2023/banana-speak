0939ebaa5659c1e342a4a5c9daad46c9a6328ff3
Hritik2023 <raihritik19898@gmail.com>
2023-04-27 19:01:18 +0530

    JS added

--- app.js
+++ app.js
@@ -3,3 +3,28 @@ var btnTranslate = document.querySelector("#btm-translate");
 var textoutput = document.querySelector("#text-output");
 
 var serverUrl = "https://api.funtranslations.com/translate/minion.json"
+
+
+function translatedUrl (textGiven){
+   var urlTranslate = serverUrl+ "?" + "text=" + textGiven.value ;
+   return(urlTranslate);
+
+}
+
+function errorHandler(error){
+    console.log("Error occured : ",error);
+    alert("error occured : "+error+"\n please try after some time");
+
+
+}
+
+
+btnTranslate.addEventListener("click",function(){
+    console.log("Button clicked");
+    var urlFinal = translatedUrl(textInput);
+    console.log(urlFinal);
+    fetch(urlFinal)
+    .then(Response => Response.json())
+    .then(json =>{ textoutput.innerText = json.contents.translated })
+    .catch(errorHandler);
+})
