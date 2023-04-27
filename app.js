aeffcec25d7ef0fa8ea59cd6a3f7c5cd0d929899
Hritik2023 <raihritik19898@gmail.com>
2023-04-27 18:20:34 +0530

    style done

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ var textInput = document.querySelector("#text-input");
 var btnTranslate = document.querySelector("#btm-translate");
 var textoutput = document.querySelector("#text-output");
 
-
+var serverUrl = "https://api.funtranslations.com/translate/minion.json"
